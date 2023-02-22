3d2e4f48897e4082e99a6b39dd136c52e44457fa
marcodedemo <marco.dedemo@gmail.com>
2023-02-22 13:22:42 +0100

    feat: script

--- js/script.js
+++ js/script.js
@@ -14,3 +14,30 @@
 //     °pago il prezzo pieno
 
 // Output con 2 cifre decimali
+
+
+let chilometri = parseInt(prompt("Quanti chilometri vuoi percorre?"));
+
+let anniPasseggero = prompt("Quanti anni hai?");
+
+let prezzoPieno = chilometri * 0.21;
+
+let prezzoScontato;
+
+
+if(anniPasseggero < 18){
+    
+    prezzoScontato = prezzoPieno - (prezzoPieno / 100 * 20);
+    
+    console.log("In quanto a minorenne hai diritto ad uno sconto del 20%, il costo del tuo biglietto è di " + prezzoScontato.toFixed(2) + "€");
+
+} else if(anniPasseggero > 65){
+
+    prezzoScontato = prezzoPieno - (prezzoPieno / 100 * 40);
+
+    console.log("In quanto a over 65 hai diritto ad uno sconto del 40%, il costo del tuo biglietto è di " + prezzoScontato.toFixed(2) + "€");
+
+} else{
+
+    console.log("Il prezzo del tuo biglietto è " + prezzoPieno.toFixed(2) + "€");
+}
