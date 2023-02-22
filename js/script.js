13ed5a8a5a6cd313f86f1a133a9d049b580e9972
marcodedemo <marco.dedemo@gmail.com>
2023-02-22 13:05:29 +0100

    init + pseudo codice

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+// Chiedo il numero di chilometri da percorre e lo memorizzo
+// Chiedo l'età del passeggero e la memorizzo
+// Calcolo il prezzo sapendo che ogni km costa 0.21€
+
+// ? SE il passeggero è minorenne
+//     °applico uno sconto del 20%
+// :? ALTRIMENTI SE il passeggero ha più di 65 anni 
+//     °applico uno sconto del 40%
+// : ALTRIMENTI 
+//     °pago il prezzo pieno
+
+// Output con 2 cifre decimali
